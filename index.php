4624bdbce5a28022ebc4419afb4508a4fa4e98db
alon-bendavid <ben-david.alon@laplateforme.io>
2023-02-01 13:00:47 +0100

    for now its finished

--- index.php
+++ index.php
@@ -13,19 +13,21 @@
 
 <body>
     <main>
+        <!-- <img class="ski" src="ski.png" alt=""> -->
         <section class="search-div">
-
+            <div class="info">
+                <h2>Ski resort search</h2>
+                <p>Get an idea of the scale of ski resorts in France with this informative tool! Search for a ski resort by name and discover its size in kilometers, providing a fun and interesting fact for ski enthusiasts</p>
+            </div>
             <form id="searchForm" class="form" method="get">
-                <input class="searchTxt" type="text" placeholder="search" autofocus name="keyWord" required>
+                <input class="searchTxt" type="text" placeholder="search" autofocus name="keyWord" autocomplete="off" required>
                 <!-- <input type="submit" placeholder="search" name="search"> -->
-                <button>search</button>
+                <!-- <button>search</button> -->
             </form>
-
-            <ul id="results">
-                <li>One</li>
-                <li>Two</li>
+            <ul class="results-housing" id="results">
+                <!-- <li>One</li> -->
+                <!-- <li>Two</li> -->
             </ul>
-
         </section>
     </main>
 </body>
